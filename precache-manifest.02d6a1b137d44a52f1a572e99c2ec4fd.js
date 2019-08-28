8fe71689a78a89e1b53087732ee64e2b7a85cf7a
Colin Franceschini <colin_franceschini@gap.com>
2019-08-28 11:49:58 -0700

    Updates

---
+++ precache-manifest.02d6a1b137d44a52f1a572e99c2ec4fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdee4999222c9dcab2c04bacc2d6e6de",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "3bee60f5490419d61dfd",
+    "url": "/tic-tac-toe/static/css/main.b6a12f34.chunk.css"
+  },
+  {
+    "revision": "4a28e03e3f80bb523f01",
+    "url": "/tic-tac-toe/static/js/2.1fdfa2b6.chunk.js"
+  },
+  {
+    "revision": "3bee60f5490419d61dfd",
+    "url": "/tic-tac-toe/static/js/main.0a4b63e9.chunk.js"
+  },
+  {
+    "revision": "97e640ec0afe7d690a63",
+    "url": "/tic-tac-toe/static/js/runtime~main.f9de6c25.js"
+  }
+]);
